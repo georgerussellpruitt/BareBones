7556f7bf82f57ebd0214fd32acd2ae6642847038
George Russell Pruitt <pruitt.russell@gmail.com>
2014-06-04 19:57:51 -0400

    Update ActionController a little more small tweaks

--- lib/functions.php
+++ lib/functions.php
@@ -3,47 +3,18 @@
 
 /*
  * debug_dump($data)
- *
  * processes an array into html for debugging
- *
 **/
-function debug_dump($data,$skip_null = false){
+function debug_dump($data,$name = false,$skip_null = false){
 	$output = "\t<div id='debugging' class='debug'>\n\r";
-	$output .= "\t\t<h3>Debugging Info</h3>\n\r";
+	if($name){
+		$output .= "\t\t<h3>Debugging Info for: $name</h3>\n\r";
+	} else {
+		$output .= "\t\t<h3>Debugging Info</h3>\n\r";
+	}
 	if ( is_string($data) ) {
 		$output = "<pre>$data</pre>\n";
-	} else if ( is_object($data) ) {
-		if($skip_null){
-			$output .= "\t\t<span>All null values being skipped</span>\n\r";
-		}
-		foreach ($data as $key => $value) {
-			if( $skip_null ){
-				if( !empty($value) ) {
-					$output .= "\t\t[<strong>".$key."</strong>]: [".$value."] \n\r";
-				}
-			} else {
-				$output .= "\t\t[<strong>".$key."</strong>]: [".$value."] \n\r";
-			}
-		}
-		$output .= "\t</div>\n\r";
-	}	else {
-		if ( is_array(@$data[0]) ) {
-			foreach($data as $array) {
-				if($skip_null){
-					$output .= "\t\t<span>All null values being skipped</span>\n\r";
-				}
-				foreach ($data as $key => $value) {
-					if( $skip_null ){
-						if( !empty($value) ) {
-							$output .= "\t\t[<strong>".$key."</strong>]: [".$value."] \n\r";
-						}
-					} else {
-						$output .= "\t\t[<strong>".$key."</strong>]: [".$value."] \n\r";
-					}
-			}
-				$output .= "\t</div>\n\r";
-			}
-		} else { // parse as $key => $value	
+	} else { // parse as $key => $value	
 			if($skip_null){
 				$output .= "\t\t<span>All null values being skipped</span>\n\r";
 			}
@@ -58,43 +29,29 @@ function debug_dump($data,$skip_null = false){
 			}
 			$output .= "\t</div>\n\r";
 		}
-	}
-	
 	return nl2br($output);
-	
 	//return $output;
 }
 
-/**
- *
- * parse_path()
- *
- * used by request controller
- *
+/*
+ * get_var_name($data)
+ * searches var globals for a match (only works on global scope)
 **/
+function get_var_name(&$var) {
+	global $GLOBALS;
+	$ret = '';
+	$tmp = $var;
+	$var = md5(uniqid(rand(), TRUE));
 
-function parse_path() {
-  $path = array();
-  if (isset($_SERVER['REQUEST_URI'])) {
-    $request_path = explode('?', $_SERVER['REQUEST_URI']);
+	$key = array_keys($GLOBALS);
+	foreach ( $key as $k )
+	if ( $GLOBALS[$k] === $var ) {
+	$ret = $k;
+	break;
+	}
 
-    $path['base'] = rtrim(dirname($_SERVER['SCRIPT_NAME']), '\/');
-    $path['call_utf8'] = substr(urldecode($request_path[0]), strlen($path['base']) + 1);
-    $path['call'] = utf8_decode($path['call_utf8']);
-    if ($path['call'] == basename($_SERVER['PHP_SELF'])) {
-      $path['call'] = '';
-    }
-    $path['call_parts'] = explode('/', $path['call']);
-
-    $path['query_utf8'] = urldecode($request_path[1]);
-    $path['query'] = utf8_decode(urldecode($request_path[1]));
-    $vars = explode('&', $path['query']);
-    foreach ($vars as $var) {
-      $t = explode('=', $var);
-      $path['query_vars'][$t[0]] = $t[1];
-    }
-  }
-	return $path;
+	$var = $tmp;
+	return $ret;
 }
 
 
